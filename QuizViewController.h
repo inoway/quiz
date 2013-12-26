e44adc9e3339daba6fb38caeb9b18c332dbc6587
Inoue Masanobu <s1531003@eng.kitakyu-u.ac.jp>
2013-12-26 15:49:37 +0900

    quiz

--- QuizViewController.h
+++ QuizViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Problem.h"
 
 @interface QuizViewController : UIViewController
 
--- QuizViewController.m
+++ QuizViewController.m
@@ -54,7 +54,7 @@ IBOutlet UITextView *problemText;
 
 -(void)loadProblemSet{
     //ファイルの読み込み
-    NSString* pass = [[NSBundle mainBundle] pathForResource:@"quiz" ofType:@"txt"];
+    NSString* path = [[NSBundle mainBundle] pathForResource:@"quiz" ofType:@"txt"];
     
     NSError* error = nil;
     int enc = NSUTF8StringEncoding;
@@ -63,7 +63,50 @@ IBOutlet UITextView *problemText;
     //行ごとに分割し、配列「line」に格納
     NSArray* lines = [text componentsSeparatedByString:@"\n"];
     
+    //問題を格納する可変配列のproblemSetを初期化
+    problemSet=[[NSMutableArray alloc]init];
+    
+    //問題の数だけ繰り返し
+    for (int i=0; i<[lines count]; i++) {
+        
+        //問題をカンマで区切って、要素を配列「items」に格納
+        NSArray* items=[[lines objectAtIndex:i] componentsSeparatedByString:@","];
+        
+        //Problemクラスのインスタンスを生成•初期化し問題文と答え格納
+        Problem *p=[Problem initProblem];
+        NSString *q=[items objectAtIndex:0];
+        int a =[[items objectAtIndex:1]intValue];
+        [p setQ:q withA:a];
+        
+        //新たに生成したProblemクラスのインスタンスをproblemSetに追加
+        [problemSet addObject:p];
+    }
 }
+
+//問題文をシャッフル
+- (void)shuffleProblemSet {
+    
+    //problemSetに格納された全問題の数を取得
+    int totalQuestions=[problemSet count];
+    
+    //Fisher-Yatesアルゴリズム用のカウンターを取得
+    int i=totalQuestions;
+    
+    //Fisher-Yatesアルゴリズムによって配列の要素をシャッフル
+    while (i>0) {
+        //乱数を発生
+        srand((unsigned int)time(0));
+        int j=rand()%i;
+        
+        //要素を並び替え
+        [problemSet exchangeObjectAtIndex:(i-1) withObjectAtIndex:j];
+        //カウンターを減少させる
+        i=i-1;
+    }
+}
+
+- (void)
+
 
 
 @end
